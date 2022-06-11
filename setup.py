cb46d188a0ca0a2e98ef5a888b12ed2a890864c6
Viren Tanti <Virentanti16@gmail.com>
2022-06-11 13:21:48 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='matrix',
+    version='0.1',
+    license='MIT',
+    author="Viren Tanti",
+    author_email='virentanti16@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Virentanti/matrix',
+    keywords='matrix',
+
+
+)
